ec292272d0281cb1ddf1f06e4126d9798293bef0
unsignedcharcoal <ce64198@gmail.com>
2026-01-16 21:10:07 -0300

    feat: added tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["var(--font-roboto)", "sans-serif"],
+      },
+    },
+  },
+};
+export default config;
