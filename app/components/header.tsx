ff430d533edc2fede5f13895106a6ce543c62d92
unsignedcharcoal <ce64198@gmail.com>
2026-01-20 09:58:13 -0300

    fix: navbar alignment by items

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,40 +4,50 @@ import Image from 'next/image';
 
 export function NavBar() {
     return (
-        <div className="bg-(--web-black-color) rounded-xl mx-auto justify-center py-3 grid grid-cols-8 gap-10">
+        <div className="bg-(--web-black-color) rounded-xl mx-auto justify-center p-3 grid grid-cols-8 gap-10">
             <div className="col-span-2 p-5">
                 <h4>Carlos Eduardo</h4>
             </div>
             <div className="col-span-4">
-                <div className="rounded-xl item-center flex flex-row w-auto items-stretch bg-(--web-black-second-color) py-2 content-center">
+                <div className="p-2 space-x-4 rounded-xl item-center flex flex-row w-auto items-stretch bg-(--web-black-second-color) content-center">
                     {NavBarButton("/", "Inicio", true)}
                     {NavBarButton("/", "Trabalhos", false)}
                     {NavBarButton("/", "Postagens", false)}
                     {NavBarButton("/", "Postage", false)}
                 </div>
             </div>
-            <div className="col-span-2 py-2 content-center">
-                <div className='grid grid-cols-2 flex flex-no-wrap grid-rows-1'>
-                    <div className='px-5 py-4 mx-2 rounded-xl font-md text-center text-md border bg-gray-600/40 border-(--web-yellow-color)'>
-                        {
-                            <Link
-                                href={""}
-                            >
-
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" className="icon icon-tabler icons-tabler-outline icon-tabler-download"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg>
-                                <h4>
-                                    Curriculum
-
-                                </h4>
-                            </Link>
-                        }
-                    </div>
-                    <div className='px-5 py-4 mx-2 rounded-xl font-md text-center text-md bg-(--web-yellow-color) text-black font-bold'>
+            <div className="col-span-2 content-center">
+                <div className='grid grid-cols-2 flex flex-no-wrap grid-rows-1 gap-5'>
+                    <div className="p-4 rounded-xl bg-gray-600/40 border border-(--web-yellow-color) flex items-center justify-center">
                         <Link
-                            className={"px-5 py-4 mx-2 rounded-xl font-md text-center text-md bg-(--web-yellow-color) text-black font-bold"}
+                            href=""
+                            className="flex items-center justify-center gap-2 text-center"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                className="icon icon-tabler icon-tabler-download"
+                            >
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+                                <path d="M7 11l5 5l5 -5" />
+                                <path d="M12 4l0 12" />
+                            </svg>
+
+                            <h4 className="text-md font-md">Curriculum</h4>
+                        </Link>
+                    </div>
+
+
+                    <div className='p-4 rounded-xl font-md text-center text-md bg-(--web-yellow-color) text-black font-bold'>
+                        <Link
                             href={""}
                         >
-                            Contato
+                            <h4 className="text-md font-md">Contato</h4>
                         </Link>
                     </div>
                 </div>
@@ -87,7 +97,7 @@ function NavBarButton(ref: string, title: string, selected: boolean): React.JSX.
 
     return (
         <Link
-            className={`px-5 py-4 mx-2 rounded-xl font-md text-center text-md ` + appendClass}
+            className={`px-5 py-3 rounded-xl font-md text-center text-md ` + appendClass}
             href={ref}
         >
             {title}
